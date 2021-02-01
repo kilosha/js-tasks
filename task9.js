3154cfee12d3c58030bc71a5f22a4e6182fd1efb
Masha <gutkovskaya18@gmail.com>
2021-02-02 00:09:19 +0300

    modified task9.js

--- task9.js
+++ task9.js
@@ -5,10 +5,10 @@
 let simple = function(string) {
     let arr = string.split(" ").map(item=> item.length).sort((a,b)=> a-b);
 
-    console.log(arr[0]);
+    return arr[0];
 }
 
 
-simple("строка в качестве примера");
-simple("пример номер два");
-simple("программировать очень интересно");
+console.log(simple("строка в качестве примера"));
+console.log(simple("пример номер два"));
+console.log(simple("программировать очень интересно"));
