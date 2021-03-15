4a9329143803c801c84c076d84b1cfadc41cb661
Masha <gutkovskaya18@gmail.com>
2021-03-15 20:52:21 +0300

    added function with Math.max and Math.min

--- task8.js
+++ task8.js
@@ -8,6 +8,9 @@ let minAndMax = arr => {
     return `${arr[0]}, ${arr[arr.length-1]}`;
 }
 
+let minAndMaxValue = arr => `${Math.min.apply(null,arr)}, ${Math.max.apply(null,arr)}`;
+
 let arr = [20, 11, 90, -1 , 0, -20, 22];
 
 console.log(minAndMax(arr));
+console.log(minAndMaxValue(arr));
